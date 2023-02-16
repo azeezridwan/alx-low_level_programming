fe0881bf1d10b28b2bff8472356be8a13db56c88
pendragonman <abdussamadowoseni28@gmail.com.com>
2023-02-16 17:39:39 +0100

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts
+ *Return:0 (success)
+*/
+
+int main(void)
+{
+
+puts("\"Programming is like a building a mutlilingual puzzle");
+return(0);
+
+}
